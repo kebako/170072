ffd6405c842d30feb2247a70f3675914ff70e6d0
勝田 賢佑 <170072@yses.ac.jp>
2018-02-02 13:01:26 +0900

    20180202

--- 20180202/MyServer01.java
+++ 20180202/MyServer01.java
@@ -3,14 +3,14 @@ import java.io.IOException;
 
 public class MyServer01{
 
-	int portNum = 6000;
+	//int portNum = 6000;
 
 	public static void main(String[] args){
 
 		ServerSocket serv = null;
 		try{
 
-			serv = new ServerSocket(this.portNum,30);
+			serv = new ServerSocket(6000,30);
 
 		}catch(IOException e){
 			System.err.println("サーバエラー");
