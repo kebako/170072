5148cc6fff6f4be7ed3dfc8f6b5417e21bd9cae6
kebako <33944572+kebako@users.noreply.github.com>
2018-02-03 10:36:55 +0900

    Update MyClient02.java

--- 20180202/MyClient02.java
+++ 20180202/MyClient02.java
@@ -1,42 +1,35 @@
 import java.io.*;
 import java.net.*;
-   
-   public class MyClient02{
-   public static void main(String[] args) throws Exception{
-      
-      byte[] buff = new byte[1024];
-      Socket writeSocket;
-      InputStream in;
-      InputStreamReader inr;
-      BufferedReader br;
-      OutputStream out;
-      PrintWriter pw = null;
-      boolean cont = true;
-   
-      try{
-        writeSocket = new Socket(172.16.21.56,4999);
-        while(cont){
-          in = writeSocket.getInputStream();
-          out = writeSocket.getOutputStream();
-        
-          InputStreamReader inr = new InputStreamReader(in);
-          BufferedReader br = new BufferedReader(inr);
-   
-          String msg = new java.util.Scanner(System.in).nextLine();
-          if(msg.equals("end")){
-            
-           cont = false;
-          }else{
-            
-          PrintWriter pw = new PrintWriter(out);
-   
-          pw.println(msg);
-          pw.flush();
-        }
-      }catch(Exception e){
-        System.err.println("ネットワークエラー");
-        System.out.println(e);
-        System.exit(1);
-      }
+
+public class MyClient02{
+  public static void main(String[] args) {
+    byte[] buff = new byte[1024];
+    Socket wrsocket = null;
+    InputStream instr = null;
+    OutputStream outstr = null;
+    String msg;
+    boolean cont = true;
+    int i;
+
+    try{
+      wrsocket = new Socket(args[0],5999);
+      instr = wrsocket.getInputStream();
+
+      msg = "ALOHA!!!!!";
+
+      outstr = wrsocket.getOutputStream();
+        //for (i=0; i<msg.length(); i++) 
+        //outstr.write((int)msg.charAt(i));
+        outstr.write('\n');
+
+      int n = instr.read(buff);
+      System.out.write(buff, 0, n);
+
+      wrsocket.close();
+    }catch (Exception e){
+      System.err.println("ネットワークエラー");
+      System.exit(1);
+      cont = false;
     }
-   }
+  }
+}
