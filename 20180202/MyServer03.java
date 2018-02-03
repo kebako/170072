ac65219c6f067ff2dbab7cb0b8dbe6553ef1e4e4
kebako <33944572+kebako@users.noreply.github.com>
2018-02-03 10:57:11 +0900

    20180203

--- 20180202/MyServer03.java
+++ 20180202/MyServer03.java
@@ -1,9 +1,6 @@
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.InetAddress;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.net.*;
+import java.io.*;
+
 
 public class MyServer03{
 	public static void main(String[] args){
@@ -12,6 +9,9 @@ public class MyServer03{
 		Socket clientSock = null;
 		OutputStream os = null;
 		PrintWriter pw = null;
+		InputStream in = null;
+		InputStreamReader inr = null;
+		BufferedReader br = null;
 
 		try{
 			servSock = new ServerSocket(5999, 30);
@@ -25,11 +25,17 @@ public class MyServer03{
 				System.out.println("クライアントIP: "+ipClient);
 
 				os = clientSock.getOutputStream();
+				in = clientSock.getInputStream();
+				inr = new InputStreamReader(in);
 				pw = new PrintWriter(os);
+				br = new BufferedReader(inr);
 
+				String clientData = br.readLine();
+				System.out.println("クライアントから受信したデータ:" + clientData);
 				pw.println("ALOHA!");
 				pw.flush();
 			}
+			//clientSock.close();
 		}catch(IOException e){
 			System.err.println("サーバエラー");
 			System.exit(1);
